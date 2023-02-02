4b4791514833c6472abee510d5d515f1681a7dbf
frandak2 <frank.montenegro@correounivalle.edu.co>
2023-02-01 19:20:45 -0500

    adding testing

--- api/app/views.py
+++ api/app/views.py
@@ -4,6 +4,9 @@ from .utils import get_model, transform_to_dataframe
 model = get_model()
 
 def get_prediction(request: PredictionRequest) -> float:
-    data_to_predict = transform_to_dataframe(request)
-    prediction = model.predict(data_to_predict)[0]
+    if all(val == 0 for val in request.values()):
+        prediction = 0
+    else:
+        data_to_predict = transform_to_dataframe(request)
+        prediction = model.predict(data_to_predict)[0]
     return max(0, prediction)
